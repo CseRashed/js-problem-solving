d24a28a8e277a4f3673b23c8aad7f849b5408629
CseRashed <cserashedul@gmail.com>
2025-10-07 08:33:14 +0600

    solving problem_1

---
+++ Problems/problem_1.js
@@ -0,0 +1,8 @@
+// Check Even or Odd (use ternary operator)
+const a = 7;
+(a % 2 ==0)?console.log(`This number is Even: ${a}`):console.log(`This number is Odd: ${a}`);
+
+// ---Sample Input/Output:
+
+// Input: 7 → Output: Odd
+// Input: 10 → Output: Even

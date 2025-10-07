578d4e1cea2e2d0155366deeab3e8345b7193de6
CseRashed <cserashedul@gmail.com>
2025-10-07 10:26:58 +0600

    solving problem_11

---
+++ Problems/problem_11.js
@@ -0,0 +1,9 @@
+// Check Multiple of 3 or 7 (use ternary operator)
+
+const num= 12;
+(num % 7 ==0)?console.log('Multiple of 7') :(num % 3==0)?console.log('Multiple of 3'):console.log('Not Multiple 7 or 3')
+
+// -----Sample Input/Output:
+
+// Input: 21 → Output: Multiple of 7
+// Input: 9 → Output: Multiple of 3

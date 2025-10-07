dbd8a788758ddd37cfd9f40bb1b61b1557509064
CseRashed <cserashedul@gmail.com>
2025-10-07 10:12:36 +0600

    solving problem_10

---
+++ Problems/problem_10.js
@@ -0,0 +1,13 @@
+//  Check Vowel or Consonant
+
+const char ='b';
+if(char =='A' || char=="a" || char == 'E' || char =='e' || char =='I' || char =='i' || char =="O" || char =="o" || char =='U' || char == "u" ){
+ console.log('Vowel')
+}else{
+    console.log('Consonant')
+}
+
+//---- Sample Input/Output:
+
+// Input: a → Output: Vowel
+// Input: b → Output: Consonant

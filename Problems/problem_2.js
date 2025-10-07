117055cc3c67772c0790058325d16b2a3063ab14
CseRashed <cserashedul@gmail.com>
2025-10-07 08:46:55 +0600

    solving problem_2

---
+++ Problems/problem_2.js
@@ -0,0 +1,16 @@
+// Check Positive, Negative or Zero
+
+const a= -5;
+if(a>0){
+    console.log(`This Number is Positive: ${a}`)
+}else if(a < 0){
+    console.log(`This Number is Negative: ${a}`)
+}else{
+    console.log(`This Number is Zero: ${a}`)
+}
+
+// ------Sample Input/Output:
+
+// Input: -5 → Output: Negative
+// Input: 0 → Output: Zero
+// Input: 12 → Output: Positive

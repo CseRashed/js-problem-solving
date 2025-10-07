39b48a103366942ea0c15a64ce20fe9225cb0cde
CseRashed <cserashedul@gmail.com>
2025-10-07 10:55:14 +0600

    solving problem_13

---
+++ Problems/problem_13.js
@@ -0,0 +1,8 @@
+// Absolute Value (use ternary operator)
+const num =-6;
+(num<0)?console.log(num*-1):console.log(num)
+
+// ----Sample Input/Output:
+
+// Input: -8 → Output: 8
+// Input: 12 → Output: 12

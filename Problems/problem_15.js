9e26eeb1b63f9d2d1dae9f80347e4f29c3742033
CseRashed <cserashedul@gmail.com>
2025-10-07 11:12:09 +0600

    solving problem_15

---
+++ Problems/problem_15.js
@@ -0,0 +1,9 @@
+// Check Profit or Loss
+const cp= 290;
+const sp= 250;
+(sp>cp)?console.log(`Profite: ${sp-cp}TK`):console.log(`Loss: ${cp-sp}TK`)
+
+// ---Sample Input/Output:
+
+// Input: CP=100 SP=150 → Output: Profit
+// Input: CP=200 SP=150 → Output: Loss

c39bf67a5905070fb596d62a9d2797fddf6abb8f
CseRashed <cserashedul@gmail.com>
2025-10-07 09:30:08 +0600

    solving problem_6

---
+++ Problems/problem_6.js
@@ -0,0 +1,9 @@
+// Check Voting Eligibility (use ternary operator)
+
+const age =15;
+(age>=18)?console.log('Eligible'):console.log('Not Eligible')
+
+// -----Sample Input/Output:
+
+// Input: 16 → Output: Not Eligible
+// Input: 20 → Output: Eligible

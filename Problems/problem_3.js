25e24e2580e78484598671dc019dcbf8407b24cf
CseRashed <cserashedul@gmail.com>
2025-10-07 08:56:51 +0600

    solving problem_3

---
+++ Problems/problem_3.js
@@ -0,0 +1,7 @@
+// Find Maximum of Two Numbers (use ternary operator)
+const num_1 =5;
+const num_2 = 9;
+(num_1 >num_2)? console.log(`Maximam Number is: ${num_1}`):console.log(`Maximam Number is: ${num_2}`)
+
+//------- Sample Input/Output:
+// Input: 10 20 → Output: 20

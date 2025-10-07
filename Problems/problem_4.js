2453489d0c7f5f96afc3e9eb5fe4abaed9a80a37
CseRashed <cserashedul@gmail.com>
2025-10-07 09:09:02 +0600

    solving problem_4

---
+++ Problems/problem_4.js
@@ -0,0 +1,15 @@
+//  Find Maximum of Three Numbers
+const num_1= 50;
+const num_2 =110;
+const num_3 =15;
+if(num_1>num_2 && num_1>num_3){
+    console.log(`Maximum Number is: ${num_1}`)
+}else if(num_2>num_3){
+    console.log(`Maximum Number is: ${num_2}`)
+}else{
+    console.log(`Maximum Number is: ${num_3}`)
+}
+
+// ----Sample Input/Output:
+
+// Input: 5 12 8 → Output: 12

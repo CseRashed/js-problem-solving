716e9ea25ec3fa79541aad31969b13f68d0bfd63
CseRashed <cserashedul@gmail.com>
2025-10-07 09:35:54 +0600

    solving problem_7

---
+++ Problems/problem_7.js
@@ -0,0 +1,9 @@
+// Check Divisibility by 5 and 11
+
+const num = 110;
+(num % 5==0 && num % 11==0)?console.log('Divisible'):console.log('Not Divisible')
+
+// ----Sample Input/Output:
+
+// Input: 55 → Output: Divisible
+// Input: 25 → Output: Not Divisible

cf8dace1872c84cd0028b8a2502473d5288eef58
CseRashed <cserashedul@gmail.com>
2025-10-07 11:05:28 +0600

    solving problem_14

---
+++ Problems/problem_14.js
@@ -0,0 +1,10 @@
+// Check Triangle Validity
+const height = 7;
+const length=4;
+const width=5;
+(height+length > width && length+width> height && height+width> length)?console.log('Valid'):console.log('Not Valid')
+
+// ----Sample Input/Output:
+
+// Input: 3 4 5 → Output: Valid
+// Input: 1 2 3 → Output: Not Valid

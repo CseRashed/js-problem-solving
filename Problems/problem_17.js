293e3ba4d595083a470109e1fa3c02cc74b194f3
CseRashed <cserashedul@gmail.com>
2025-10-07 11:20:43 +0600

    solving problem_17

---
+++ Problems/problem_17.js
@@ -0,0 +1,9 @@
+//  Check Pass or Fail (use ternary operator)
+
+const mark = 50;
+(mark>=40)?console.log('Pass'):console.log('Fail')
+
+// Sample Input/Output:
+
+// Input: 35 → Output: Fail
+// Input: 60 → Output: Pass

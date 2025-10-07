9bb855871eaff7c621e63e59f268eac8649ab01c
CseRashed <cserashedul@gmail.com>
2025-10-07 10:02:28 +0600

    solving problem_9

---
+++ Problems/problem_9.js
@@ -0,0 +1,13 @@
+//  Check Uppercase or Lowercase (use ternary operator)
+
+const character = 'f'
+if(character >='A' && character <= 'Z'){
+    console.log('UpperCase')
+}else{
+    console.log('LowerCase')
+}
+
+// ----Sample Input/Output:
+
+// Input: A → Output: Uppercase
+// Input: g → Output: Lowercase

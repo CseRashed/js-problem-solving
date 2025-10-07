3ad2da31cbd415f0f1985ab576b0d6c97d413d8a
CseRashed <cserashedul@gmail.com>
2025-10-07 09:49:46 +0600

    solving problem_8

---
+++ Problems/problem_8.js
@@ -0,0 +1,17 @@
+// Grade Calculator
+
+const mark= 30;
+if(mark>=80){
+    console.log('Grade A')
+}else if(mark>=70){
+    console.log('Grade B')
+}else if(mark>= 33){
+    console.log('Grade C')
+}else{
+    console.log('Grade F')
+}
+
+// ----Sample Input/Output:
+
+// Input: 85 → Output: Grade A
+// Input: 50 → Output: Grade C

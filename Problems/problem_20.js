6e5f7797c67b3bb0fd0934671eb5d9001499fdcd
CseRashed <cserashedul@gmail.com>
2025-10-07 11:50:17 +0600

    solving problem_20

---
+++ Problems/problem_20.js
@@ -0,0 +1,17 @@
+// Student Performance Category (if...else if)
+
+const marks= 80;
+if(marks>=90){
+    console.log('Excellent')
+}else if(marks >=75){
+    console.log('Very Good')
+}else if(marks>=50){
+    console.log('Average')
+}else{
+    console.log('Needs Improvement')
+}
+
+// ----Sample Input/Output:
+
+// Input: 92 → Output: Excellent
+// Input: 70 → Output: Average

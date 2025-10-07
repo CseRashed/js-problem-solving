7b74df222bcb5da1d898ed082632fc572bf5d91d
CseRashed <cserashedul@gmail.com>
2025-10-07 11:16:43 +0600

    solving problem_16

---
+++ Problems/problem_16.js
@@ -0,0 +1,9 @@
+//  Find Minimum of Two Numbers (use ternary operator)
+
+const num_1= 10;
+const num_2 =20 ;
+(num_1<num_2)?console.log(`Minumum Number is: ${num_1}`):console.log(`Minimum Number is: ${num_2}`)
+
+//---- Sample Input/Output:
+
+// Input: 12 5 → Output: 5
